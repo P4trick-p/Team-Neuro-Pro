ae5828cc2cf7d289621f48e2af0750bb7f290316
P4trick-p <3527184+P4trick-p@users.noreply.github.com>
2020-03-22 18:46:08 +0100

    SNRv2 - wersja na tablicach 2D WIP (Work In Progress)

--- SNRv2.py
+++ SNRv2.py
@@ -1,6 +1,8 @@
 import numpy as np
 import glob
 import imageio
+# for debugging
+import time
 
 # deprecated scipy function
 def signaltonoise(a, axis=0, ddof=0):
@@ -10,18 +12,35 @@ def signaltonoise(a, axis=0, ddof=0):
     return np.where(sd == 0, 0, m/sd)
 
 # Tworzenie wektora z obrazami
-img = [0] * 50
-i = 0
 
-for image_path in glob.glob("im_base_original\Hockney\\*.jpg"):
-    im = imageio.imread(image_path)
-    img[i] = im
-    i += 1
+#art = [0]*3
+#img = [0]*50
+rows, cols = (3, 50)
+arr = [[0]*cols]*rows
 
+# 1 - Beksinski, 2 - Gogh, 3 - Hockney
+B = glob.glob("im_base_original\Beksinski\\*.jpg")
+G = glob.glob("im_base_original\Gogh\\*.jpg")
+H = glob.glob("im_base_original\Hockney\\*.jpg")
+artists = [ B , G , H]
+for x in range(rows):
+    i = 0
+    for image_path in artists[x]:
+        im = imageio.imread(image_path)
+        arr[x][i] = im
+        i += 1
+    x += 1
+x=0
+i=0
 # Liczenie SNR (Signal to Noise Ratio) // Jednostka?
-snr = [0] * 50
-for x in range(50):
-    snr[x] = signaltonoise(img[x] , axis=None )
-    print(x+1, 'SNR =' , snr[x])
-avg = np.average(snr)
-print('Average SNR = ' , avg)
+avg = [0]*cols
+SNR = [[0]*cols]*rows
+for x in range(rows):
+    for i in range(cols):
+        img = arr[x][i]
+        SNR[x][i] = signaltonoise(img , axis=None )
+        print(i+1, 'SNR =' , SNR[x][i])
+    snr = SNR[x]
+    avg[x] = np.average(snr)
+    print('Average SNR = ' , avg)
+    time.sleep(5)
