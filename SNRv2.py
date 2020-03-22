931cf2bc14c02445d9937aa777086a6e426fe909
P4trick-p <3527184+P4trick-p@users.noreply.github.com>
2020-03-22 22:41:33 +0100

    SNRv2 - wersja na tablicach 2D WIP (Work In Progress)

--- SNRv2.py
+++ SNRv2.py
@@ -1,31 +1,22 @@
 import numpy as np
 import glob
 import imageio
-# for debugging
-import time
+import time # for debugging
 
-# deprecated scipy function
-def signaltonoise(a, axis=0, ddof=0):
+def signaltonoise(a, axis=0, ddof=0): # deprecated scipy function
     a = np.asanyarray(a)
     m = a.mean(axis)
     sd = a.std(axis=axis, ddof=ddof)
     return np.where(sd == 0, 0, m/sd)
-
-# Tworzenie wektora z obrazami
-
-#art = [0]*3
-#img = [0]*50
+# Tworzenie tablicy 2D z artystami i ich obrazami
 rows, cols = (3, 50)
 arr = [[0]*cols]*rows
+#art = ( glob.glob("base_original\Beksinski\\*.jpg") , glob.glob("base_original\Hockney\\*.jpg") , glob.glob("base_original\Gogh\\*.jpg"))
+art = ( glob.glob("base_original\Beksinski\\*.jpg") , glob.glob("base_original\Hockney\\*.jpg") , glob.glob("base_original\Gogh\\*.jpg"))
 
-# 1 - Beksinski, 2 - Gogh, 3 - Hockney
-B = glob.glob("im_base_original\Beksinski\\*.jpg")
-G = glob.glob("im_base_original\Gogh\\*.jpg")
-H = glob.glob("im_base_original\Hockney\\*.jpg")
-artists = [ B , G , H]
 for x in range(rows):
     i = 0
-    for image_path in artists[x]:
+    for image_path in art[x]:           # Coś tu nie gra
         im = imageio.imread(image_path)
         arr[x][i] = im
         i += 1
@@ -40,7 +31,9 @@ for x in range(rows):
         img = arr[x][i]
         SNR[x][i] = signaltonoise(img , axis=None )
         print(i+1, 'SNR =' , SNR[x][i])
+        i += 1
     snr = SNR[x]
     avg[x] = np.average(snr)
-    print('Average SNR = ' , avg)
-    time.sleep(5)
+    print('Average SNR = ' , avg[x])
+    time.sleep(2)
+    x += 1
